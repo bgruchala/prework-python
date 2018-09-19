cbf3e1def5bb03d381756926102906db078fa1cb
Bartłomiej Gruchała <43046270+bgruchala@users.noreply.github.com>
2018-09-19 21:47:22 +0200

    Initial commit

---
+++ intro-1.py
@@ -0,0 +1,62 @@
+# Intro to Python - 1
+
+# Python Basis
+
+# ex 2
+print(5 / 8)
+print(7 + 10)
+
+# ex 5
+print(5 + 5)
+print(5 - 5)
+
+print(3 * 5)
+print(10 / 2)
+print(18 % 7)
+print(4 ** 2)
+
+print(100*1.1**7)
+
+# ex 7
+savings = 100
+
+print(savings)
+
+# ex 8
+savings = 100
+factor = 1.10
+
+result = savings * factor ** 7
+
+print(result)
+
+# ex 9
+desc = "compound interest"
+
+profitable = True
+# ex 11
+savings = 100
+factor = 1.1
+desc = "compound interest"
+
+year1 = savings * factor
+
+print(type(year1))
+
+doubledesc = desc + desc
+
+print(doubledesc)
+
+# ex 12
+savings = 100
+result = 100 * 1.10 ** 7
+
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
+
+pi_string = "3.1415926"
+
+pi_float = float(pi_string)
+
+# Python Lists
+
+#
